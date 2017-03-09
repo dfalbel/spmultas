83a6ea7a2142fac335b24af67e13e3e958e6ed6f
Daniel Falbel <dfalbel@gmail.com>
2017-03-09 15:30:36 -0300

    arrumando a documentação

---
+++ R/doc.R
@@ -0,0 +1,15 @@
+#' Infrações cometidas por carros capturadas eletronicamente (radar)
+#'
+#' Banco de dados extraído manualmente do portal da mobilidade urbana.
+#' \url{http://mobilidadesegura.prefeitura.sp.gov.br}
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{data}{data da infração}
+#'   \item{hora}{hora do dia da infração}
+#'   \item{enquadramento}{motivo da multa}
+#'   \item{local}{local da infração}
+#'   \item{qtd}{quantidade de infrações}
+#' }
+#' @source \url{http://mobilidadesegura.prefeitura.sp.gov.br}
+"carros_eletronicas"
