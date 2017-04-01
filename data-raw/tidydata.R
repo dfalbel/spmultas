8de438bd9ecfc1db057f80d1dbbaae38a98df71c
Daniel Falbel <dfalbel@gmail.com>
2017-04-01 16:23:08 -0300

    codigo melhor e correcao

--- data-raw/tidydata.R
+++ data-raw/tidydata.R
@@ -11,23 +11,40 @@ library(tidyr)
 
 # base de multas de forma eletrônica
 arqs <- list.files("data-raw/carros/eletronica/", full.names = TRUE, recursive = TRUE)
-# alguns arquivos são excel e outros txt (por isso essa função complicadinha)
+
+csv_reader <- function(path, col_names){
+  read_delim(path, ";", escape_double = FALSE, trim_ws = TRUE,
+             col_names = col_names, skip = 1, progress = FALSE
+             )
+}
+
+excel_reader <- function(path, col_names){
+  read_excel(path, col_names = col_names, skip = 1)
+}
+
+reader <- function(path, col_names){
+  if(str_detect(path, "xls$")){
+    excel_reader(path, col_names)
+  } else {
+    csv_reader(path, col_names)
+  }
+}
+
 bases <- data_frame(arqs = arqs) %>%
   filter(str_detect(arqs, "[xls|csv]$")) %>%
-  mutate(dados = map(arqs, failwith(NA, function(arq){
-    nomes <- c("data", "enquadramento", "local", "qtd")
-    if(str_detect(arq, "xls$")){
-      read_excel(arq, col_names = nomes, skip = 1)
-    } else {
-      read_delim(arq, ";", escape_double = FALSE, trim_ws = TRUE,
-                 col_names = nomes, skip = 1)
-    }
-  })))
+  mutate(
+    dados = map(arqs, failwith(NA, function(path){
+      nomes <- c("data", "enquadramento", "local", "qtd")
+      reader(path, nomes)
+    }))
+  )
+
 bases <- bases %>% unnest(dados)
 bases <- bases %>% filter(!is.na(qtd) & !is.na(data))
+
 carros_eletronicas <- bases %>%
   mutate(
-    hora = str_match(arqs, "([0-9]{2}).xls")[,2] %>% as.integer(),
+    hora = str_match(arqs, "([0-9]{2}).[xls|csv]")[,2] %>% as.integer(),
     data = lubridate::dmy(data)
     ) %>%
   select(data, hora, enquadramento, local, qtd)
